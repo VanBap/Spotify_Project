45029580aea3995b68c02d6b2e4dad1ef5b81c4e
VanBap <vanbap20021599@gmail.com>
2023-03-30 23:28:59 +0700

    update

---
+++ src/Components/LeftMenu.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../Styles/LeftMenu.css';
+
+function LeftMenu()
+{
+  return <div className="LeftMenu">
+    <div className="logoContainer"></div>
+    <div className="searchBox"></div>
+  </div>;
+}
+export {LeftMenu};
